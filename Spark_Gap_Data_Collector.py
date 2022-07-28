c935c9e1ab3dd4f21de2ae96c14811496d44387a
3-3-3 <l_keeley@coloradocollege.edu>
2022-07-28 13:03:59 -0700

    Including data to easily go between computers

--- Spark_Gap_Data_Collector.py
+++ Spark_Gap_Data_Collector.py
@@ -4,29 +4,35 @@ from Keysight_Methods import get_data, get_data_channels
 import os
 import sys
 import matplotlib.pyplot as plt
+from datetime import date
 
 
 if __name__ == '__main__':
     args = sys.argv
+    today = date.today()
 
-    save_dir = 'NoLED'
+    save_dir = 'Breakdown_Measurements'
+    sub = f'G0.05mm_V2310V_{today.month}_{today.day}'
+    dir = os.path.join(save_dir, sub)
     trial = 1
-    f_name = os.path.join(save_dir, f'trial_{trial}.npy')
+    f_name = os.path.join(dir, f'trial_{trial}.npy')
+
 
     try:
-        os.mkdir(save_dir)
+        os.mkdir(dir)
     except:
         pass
 
     while os.path.exists(f_name):
         trial += 1
-        f_name = os.path.join(save_dir, f'trial_{trial}.npy')
+        f_name = os.path.join(dir, f'trial_{trial}.npy')
 
 
     rm = pyvisa.ResourceManager()
     addy = rm.list_resources()[0]
     inst = rm.open_resource(addy)
-    channels = [2,3]
+    inst.timeout = 15000
+    channels = [3,4]
 
     if len(args) > 1:
         if args[1] == 'verbose':
@@ -40,11 +46,11 @@ if __name__ == '__main__':
             print(f'[*] Closing instance: {inst}')
 
             print(f'[*] Displaying data')
+
             for channel in data[1:data.shape[0]]:
                 plt.plot(data[0], channel)
                 plt.grid()
-
-            plt.show()
+                plt.show()
 
     else:
         data = get_data_channels(inst, channels=channels)
