b99ed25e340f77b55fce1605dc5f7b0228ff6d4e
3-3-3 <l_keeley@coloradocollege.edu>
2022-07-28 09:37:04 -0700

    Added some documentation in codee

--- Keysight_Methods.py
+++ Keysight_Methods.py
@@ -3,40 +3,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def get_data_channels(inst, points='MAX', channels=[1]):
-    #Set
-    inst.write(f':WAVeform:POINts {points}')
-    points = int(inst.query(f':WAVeform:POINts?'))
+    '''
+    Method to load data from multiple channels on Keysight Oscilliscope 3000-x
+    (tested on MSOX3104T) into numpy array
+
+    inst: pyvisa instrument instance of keysight oscilliscope
+    points: number of points to collect. Default is the maximum allowed.
+    channels: list of channels to collect from. Default is the first channel.
+
+    Returns: Numpy array with rows: time, channel 1, channel 2...
+    '''
+
     data = np.array([])
 
     for channel in channels:
         inst.write(':WAVeform:POINts:MODE RAW')
+        inst.write(f':WAVeform:POINts {points}')
         inst.write(f':WAVeform:SOURce CHANnel{channel}')
+        #Pull  binary data, and then convert on computer for speed
         inst.write(':WAVeform:FORMat BYTE')
 
-
+        #Includes information for recovering voltage and time data
+        #from raw binary data
+        #see https://www.keysight.com/us/en/assets/9018-06894/programming-guides/9018-06894.pdf?success=true
+        #pp. 1008
         preamble = inst.query(':WAVeform:PREamble?')
-        (wav, acq, wfmpts,
+        (format, acq_type, n_points,
             avgcnt, t_inc, t_0,
-            x_ref, y_inc, y_origin, y_ref) = [float(i) for i in preamble.split(",")]
+            t_ref, y_inc,
+            y_0, y_ref) = [float(i) for i in preamble.split(",")] #unpack information
 
         d = np.array(inst.query_binary_values(':WAVeform:DATA?', datatype='s'))
-        d = (d - y_ref) * y_inc + y_origin
+        d = (d - y_ref) * y_inc + y_0
 
         if channel == channels[0]:
             data = d
         else:
             data = np.vstack((data, d))
 
-
+        #Each channel has the same time scale
+        #so only do this once
         if channel == channels[-1]:
-            t_max = t_inc * data.size
+            t_max = (t_inc - t_ref) * data.shape[1] + t_0
             t = np.linspace(t_0, t_max, data.shape[1])
 
             data = np.vstack((t,data))
 
-    return data.transpose()
+    return data
 
 def get_data(inst, points='MAX', channel=1):
+    '''
+    Method to load data a single channels on Keysight Oscilliscope 3000-x
+    (tested on InfiniiVision MSOX3104T) into numpy array
+
+    inst: pyvisa instrument instance of keysight oscilliscope
+    points: number of points to collect. Default is the maximum allowed.
+    channel: channel to pull data from.
+
+    Returns: Numpy array with rows: time, channel
+    '''
+
     inst.write(':WAVeform:POINts:MODE RAW')
     inst.write(f':WAVeform:POINts {points}')
     inst.write(f':WAVeform:SOURce CHANnel{channel}')
--- Spark_Gap_Data_Collector.py
+++ Spark_Gap_Data_Collector.py
@@ -2,9 +2,13 @@ import pyvisa
 import numpy as np
 from Keysight_Methods import get_data, get_data_channels
 import os
+import sys
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
+    args = sys.argv
+
     save_dir = 'NoLED'
     trial = 1
     f_name = os.path.join(save_dir, f'trial_{trial}.npy')
@@ -17,18 +21,32 @@ if __name__ == '__main__':
     while os.path.exists(f_name):
         trial += 1
         f_name = os.path.join(save_dir, f'trial_{trial}.npy')
-        
+
 
     rm = pyvisa.ResourceManager()
     addy = rm.list_resources()[0]
     inst = rm.open_resource(addy)
     channels = [2,3]
 
-    print(f'[*] Getting data from channels: {channels}')
-    data = get_data_channels(inst, channels=channels)
+    if len(args) > 1:
+        if args[1] == 'verbose':
+            print(f'[*] Getting data from channels: {channels}')
+            data = get_data_channels(inst, channels=channels)
 
-    print(f'[*] Writing data to: {f_name}')
-    with open(f_name, 'wb') as f:
-        np.save(f, data)
+            print(f'[*] Writing data to: {f_name}')
+            with open(f_name, 'wb') as f:
+                np.save(f, data)
 
-    print(f'[*] Closing instance: {inst}')
+            print(f'[*] Closing instance: {inst}')
+
+            print(f'[*] Displaying data')
+            for channel in data[1:data.shape[0]]:
+                plt.plot(data[0], channel)
+                plt.grid()
+
+            plt.show()
+
+    else:
+        data = get_data_channels(inst, channels=channels)
+        with open(f_name, 'wb') as f:
+            np.save(f, data)
