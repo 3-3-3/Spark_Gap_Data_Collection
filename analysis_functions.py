f3b62ece01f3ff3888cac8ea2a2f891ba657ec81
3-3-3 <l_keeley@coloradocollege.edu>
2022-07-29 11:22:24 -0600

    Did some data analysis

---
+++ analysis_functions.py
@@ -0,0 +1,79 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+def dt(t, V, threshold=-0.4):
+    '''
+    Return dt between discharges
+    given a voltage array and a time array
+    V: voltage array
+    t: time array
+    '''
+    dV = np.diff(V)
+    t = t[1:len(t)]
+
+    dt = []
+    t_last = 0
+
+    points = 0
+    delta_V = 0
+
+    for i in range(len(dV)):
+        if np.sign(dV[i]) == -1:
+            delta_V += dV[i]
+            points += 1
+
+        else:
+            if delta_V < threshold:
+                #Do not use first measurement
+                if t_last != 0:
+                    dt.append(t[i-points] - t_last)
+                t_last = t[i-points]
+
+            delta_V = 0
+            points = 0
+
+    return np.array(dt)
+
+def dt_from_measurement_dir(dir):
+    d = np.array([])
+    for file in os.listdir(dir):
+        data = np.load(os.path.join(dir, file))
+        d = np.append(d,dt(data[0],data[1]))
+
+    return d
+
+def dt_hist(dt, v, gap, color, bins):
+    plt.style.use('ggplot')
+    plt.title(f'Discharge Distribution At {v} V and {gap} mm Gap')
+    plt.xlabel('dt (s)')
+    plt.ylabel('Count')
+    plt.hist(dt,bins=bins,color=color,edgecolor='black')
+
+
+    plt.savefig(f'v{v}_{gap}mm.png')
+    plt.show()
+
+def frames(V, threshold = -0.4):
+    frames = []
+    dV = np.diff(V)
+    i_1 = 0
+    i_2 = 0
+    points = 0
+    delta_V = 0
+
+    for i in range(len(dV)):
+        if np.sign(dV[i]) == -1:
+            points += 1
+            delta_V += dV[i]
+
+        else:
+            if delta_V < threshold:
+                #Detect discharge
+                frames.append(V[i_1:i_2-points])
+                i_1 = i_2
+            delta_V = 0; points = 0
+
+        i_2 += 1
+
+    return frames
